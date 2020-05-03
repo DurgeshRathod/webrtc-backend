4965bc35fb39913a839e59ccc413911d7bb2e4c5
Durgesh Rathod <durgeshrathod.777@gmail.com>
2020-05-03 19:13:56 +0530

    socket.io library used

--- src/server.ts
+++ src/server.ts
@@ -1,30 +1,53 @@
 import * as express from 'express';
 import * as http from 'http';
-import * as WebSocket from 'ws';
+import * as socket from "socket.io"
 
 const app = express();
 
 //initialize a simple http server
 const server:any = http.createServer(app);
 
-//initialize the WebSocket server instance
-const wss = new WebSocket.Server({ server });
+const io = socket(server)
+io.sockets.on('connection', (socket) => {
 
-wss.on('connection', (ws: WebSocket) => {
-
-    //connection is up, let's add a simple simple event
-    ws.on('message', (message: string) => {
-
+    console.log("connected");
+    socket.send("abcdef");
+    
+    socket.on('message', (message: string) => {
         //log the received message and send it back to the client
         console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
+        socket.send(`Hello, you sent -> ${message}`);
     });
 
-    //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server');
+
+    socket.on('JOIN_ROOM', function(room) {
+        if(socket.rooms.room){
+            socket.leave(socket.rooms.room);
+        }
+    
+        socket.join(room);
+    });
+
+    socket.on("LEAVE_ROOM",(room)=>{
+        socket.leave(room)
+        
+    })
+    socket.send("CONNECTED")
+    // now, it's easy to send a message to just the clients in a given room
+    
+    socket.on("message",(data)=>{
+        // socket.in(room).emit('message', 'what is going on, party people?');
+        sendDataInRoom(socket.rooms[Object.keys(socket.rooms)[0]] , data)
+    })
+    // io.sockets.in(room).emit('message', 'what is going on, party people?');
+
+    function sendDataInRoom(room:string, data:any){
+        io.sockets.in(room).emit('event', data);
+    }
 });
 
 //start our server
+
 server.listen(process.env.PORT || 8999, () => {
-    console.log(`Server started on port ${server.address().port} :)`);
+    console.log(`Server started on  - port ${server.address().port} :)`);
 });
